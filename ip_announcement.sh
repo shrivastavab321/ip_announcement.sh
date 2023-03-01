dd6f4280e68985616f87b10a9a7740d1b9347504
badal shrivastava <103625468+shrivastavab321@users.noreply.github.com>
2023-03-01 11:39:52 +0530

    Add files via upload

---
+++ ip_announcement.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+
+ip=$(hostname -I)
+#espeak -ven+m1 -s 130 $ip
+
+declare -a M_IP
+
+for i in $(echo $ip | fold -w1  )
+do
+if [[ $i == *.* ]] then
+espeak -p 70 "dot"
+else
+espeak -p 70 $i
+fi
+done
+echo $M_IP
